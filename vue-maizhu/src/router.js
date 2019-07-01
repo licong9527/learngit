93aab34f40438799e36393778aad2bc264381a4a
licong9527 <1291194706@qq.com>
2019-07-01 10:38:32 +0800

    初始

---
+++ vue-maizhu/src/router.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      component: () => import("./views/home/index.vue"),
+      children: [
+        {
+          path: "films",
+          component: () => import("./views/home/films.vue")
+        },
+        {
+          path: "cinemas",
+          component: () => import("./views/home/cinemas.vue")
+        },
+        {
+          path: "center",
+          component: () => import("./views/home/center.vue")
+        },
+        {
+          path: "",
+          redirect: "/films"
+        }
+      ]
+    },
+    {
+      path: "./film/:filmId",
+      component: () => import("./views/film/index.vue")
+    },
+    {
+      path: "/city",
+      component: () => import("./views/city/index.vue")
+    },
+    {
+      path: "*",
+      redirect: "/films"
+    }
+  ]
+});
---
+++ vue-maizhu/src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-home">
+    <router-view></router-view>
+    <h1>我是承载着 电影列表 影院列表 个人中心页</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "home"
+};
+</script>
