a291110394ee7b208dd53e8fa02a5f316e174918
licong9527 <1291194706@qq.com>
2019-07-02 15:14:57 +0800

    底部完成

--- vue-maizhu/src/components/Tabber/index.vue
+++ vue-maizhu/src/components/Tabber/index.vue
@@ -1,29 +1,11 @@
 <template>
   <div class="mz-tabbar">
     <ul class="mz-tabbar__ul">
-      <li class="mz-tabbar__li">
-        <a href>
-          <i>图标</i>
-          <span>电影</span>
-        </a>
-      </li>
-      <li class="mz-tabbar__li">
-        <a href>
-          <i>图标</i>
-          <span>影院</span>
-        </a>
-      </li>
-      <li class="mz-tabbar__li">
-        <a href>
-          <i>图标</i>
-          <span>特惠</span>
-        </a>
-      </li>
-      <li class="mz-tabbar__li">
-        <a href>
-          <i>图标</i>
-          <span>我的</span>
-        </a>
+      <li class="mz-tabbar__li" v-for="(tab, index) in tabs" :key="index">
+        <router-link :to="tab.to" active-class="active">
+          <i class="iconfont icon-dianying" :class="tab.icon"></i>
+          <span>{{ tab.name }}</span>
+        </router-link>
       </li>
     </ul>
   </div>
@@ -31,12 +13,23 @@
 
 <script>
 export default {
-  name: "Tabbar"
+  name: "Tabbar",
+  props: {
+    tabs: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
 };
 </script>
 <style lang="scss">
 @import "../../assets/style/common/px2rem.scss";
+@import "../../assets/style/common/mixins.scss";
 .mz-tabbar {
+  @include border-top;
+  @include border-bottom;
   height: px2rem(50);
   background-color: #fff;
   &__ul {
@@ -48,8 +41,19 @@ export default {
     a {
       display: flex;
       flex-direction: column;
+      justify-content: center;
       align-items: center;
       text-decoration: none;
+      font-size: 12px;
+      color: #797d82;
+      height: 100%;
+      .iconfont {
+        font-size: 24px;
+        margin-bottom: 4px;
+      }
+      &.active {
+        color: #ff5f16;
+      }
     }
   }
 }
--- vue-maizhu/src/views/home/index.vue
+++ vue-maizhu/src/views/home/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="page-home">
-    <Tabbar />
+    <router-view></router-view>
+    <Tabbar
+      :tabs="[
+        { name: '电影', icon: 'icon-dianying', to: '/films' },
+        { name: '影院', icon: 'icon-yingyuan', to: '/cinemas' },
+        { name: '特惠', icon: 'icon-tehui', to: '/111' },
+        { name: '我的', icon: 'icon-wode', to: '/center' }
+      ]"
+    />
   </div>
 </template>
 <script>
@@ -14,3 +22,15 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.page-home {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  > div:first-child {
+    flex: 1;
+    overflow-y: auto;
+  }
+}
+</style>
